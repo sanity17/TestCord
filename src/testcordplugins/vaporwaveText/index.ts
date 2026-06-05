9c0a951ea560ffbffcd27d46d3f05fa8a715e4f5
x2b <mokihbbjjj@gmail.com>
2026-06-06 00:35:15 +0200

    add vaporwaveText plugin

---
+++ src/testcordplugins/vaporwaveText/index.ts
@@ -0,0 +1,32 @@
+/*
+ * MallCord, a vaporwave-inspired Discord client mod
+ * Copyright (c) 2026 unfamiliardev
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { findOption, RequiredMessageOption } from "@api/Commands";
+import definePlugin from "@utils/types";
+
+/** Convert ASCII to fullwidth characters: "aesthetic" -> "ａｅｓｔｈｅｔｉｃ" */
+function toFullwidth(text: string): string {
+    return text.replace(/[ -~]/g, c =>
+        c === " " ? "　" : String.fromCharCode(c.charCodeAt(0) + 0xFEE0)
+    );
+}
+
+export default definePlugin({
+    name: "VaporwaveText",
+    description: "Adds /vaporwave to turn your message into ａｅｓｔｈｅｔｉｃ fullwidth text.",
+    authors: [{ name: "Sharp", id: 0n }],
+    dependencies: ["CommandsAPI"],
+    commands: [
+        {
+            name: "vaporwave",
+            description: "Convert text to fullwidth ａｅｓｔｈｅｔｉｃ characters",
+            options: [RequiredMessageOption],
+            execute: opts => ({
+                content: toFullwidth(findOption(opts, "message", ""))
+            })
+        }
+    ]
+});
