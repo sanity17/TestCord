25b8863c38fbb8a2ca355f6531fece419e0ecc5a
x2b <mokihbbjjj@gmail.com>
2026-06-13 19:50:12 +0200

    grammarFix: Store and remove presend listener on stop()
    Store the presend listener reference so the same function can be
    properly removed on stop(). Previously stop() tried to remove a
    different listener instance than the one that was added.

--- src/testcordplugins/grammarFix/index.ts
+++ src/testcordplugins/grammarFix/index.ts
@@ -85,16 +85,21 @@ export default definePlugin({
     authors: [TestcordDevs.x2b],
     dependencies: ["MessageEventsAPI"],
     settings,
+    _presendListener: null as MessageSendListener | null,
     async start() {
         let dictionary = await fetch(
             "https://raw.githubusercontent.com/wont-stream/dictionary/6b9d2f06a1d89103fb7249f41de4db6811e3d374/index.min.json",
         );
         dictionary = await dictionary.json();
 
-        addMessagePreSendListener(getPresend(dictionary));
+        this._presendListener = getPresend(dictionary);
+        addMessagePreSendListener(this._presendListener);
     },
     stop() {
-        removeMessagePreSendListener(getPresend({}));
+        if (this._presendListener) {
+            removeMessagePreSendListener(this._presendListener);
+            this._presendListener = null;
+        }
     },
 });
 
