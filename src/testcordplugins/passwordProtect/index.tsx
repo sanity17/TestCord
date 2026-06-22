62972295fb7f733df492867a8373e20cb32c00bf
x2b <mokihbbjjj@gmail.com>
2026-06-22 17:04:58 +0200

    chore: update src/testcordplugins/passwordProtect/index.tsx

--- src/testcordplugins/passwordProtect/index.tsx
+++ src/testcordplugins/passwordProtect/index.tsx
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { Devs, TestcordDevs } from "@utils/constants";
+import { TestcordDevs } from "@utils/constants";
 import { getCurrentChannel } from "@utils/discord";
 import definePlugin from "@utils/types";
 
@@ -84,8 +84,3 @@ export default definePlugin({
         saveData();
     },
 });
-
-
-
-
-
