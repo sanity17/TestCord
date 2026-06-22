bcf03685c2013c26d029cafe2f3edcf466b6a0a5
x2b <mokihbbjjj@gmail.com>
2026-06-22 17:05:01 +0200

    chore: update src/testcordplugins/vAnalyzer-main/native.ts

--- src/testcordplugins/vAnalyzer-main/native.ts
+++ src/testcordplugins/vAnalyzer-main/native.ts
@@ -4,15 +4,15 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-export { lookupDangeCordProfile } from "./analyzers/Dangercord/native";
 export { queryCertPL } from "./analyzers/CertPL/native";
+export { queryCordCat } from "./analyzers/CordCat/native";
 export { queryCrtSh } from "./analyzers/CrtSh/native";
-export { queryDiscordInvite, queryDiscordGuildWidget } from "./analyzers/DiscordInvite/native";
+export { lookupDangeCordProfile } from "./analyzers/Dangercord/native";
+export { queryDiscordGuildWidget,queryDiscordInvite } from "./analyzers/DiscordInvite/native";
 export { queryFishFish } from "./analyzers/FishFish/native";
 export { hybridAnalysisGetScan, hybridAnalysisHashFile, hybridAnalysisQuickScanFile, hybridAnalysisQuickScanUrl, hybridAnalysisSearchHash } from "./analyzers/HybridAnalysis/native";
+export { executeModularScan } from "./analyzers/ModularScan/native";
+export { querySucuri } from "./analyzers/Sucuri/native";
 export { getVirusTotalFileReport, lookupVirusTotalFile, makeVirusTotalRequest } from "./analyzers/VirusTotal/native";
 export { queryWayback } from "./analyzers/WaybackMachine/native";
-export { querySucuri } from "./analyzers/Sucuri/native";
 export { traceUrl } from "./analyzers/WhereGoes/native";
-export { executeModularScan } from "./analyzers/ModularScan/native";
-export { queryCordCat } from "./analyzers/CordCat/native";
