ddcfcce42b020170131e9276cdb2f3614122500d
x2b <mokihbbjjj@gmail.com>
2026-06-22 17:04:51 +0200

    chore: update src/Betterdiscordplugins/SimpleAnimations.plugin.js

--- src/Betterdiscordplugins/SimpleAnimations.plugin.js
+++ src/Betterdiscordplugins/SimpleAnimations.plugin.js
@@ -1,3 +1,9 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 /**
  * @name SimpleAnimations
  * @author Testcord Team
