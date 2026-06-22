8ce87a9496a7a87c2a52b73a183c7aca96e21ae4
x2b <mokihbbjjj@gmail.com>
2026-06-22 17:04:51 +0200

    chore: update src/previewExample.tsx

--- src/previewExample.tsx
+++ src/previewExample.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import React from "react";
 
 interface User {
     id: number;
