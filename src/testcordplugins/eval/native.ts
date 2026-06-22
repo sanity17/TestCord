65c058f827fef5f95d3b7f6e532c11af5747591c
x2b <mokihbbjjj@gmail.com>
2026-06-22 17:04:56 +0200

    chore: update src/testcordplugins/eval/native.ts

--- src/testcordplugins/eval/native.ts
+++ src/testcordplugins/eval/native.ts
@@ -1,3 +1,9 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 import { dialog } from "electron";
 
 export async function evalCode(_, code: string) {
